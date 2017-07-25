4fce58eac95f5f39b87867adb14370d056c04716
phpfs <me@perryfs,de>
2017-07-25 19:33:24 +0200

    Fixed overwrite of thold

--- services.go
+++ services.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+	"time"
+
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
-	"time"
 )
 
 func syncServices(session *mgo.Session) bool {
@@ -13,12 +14,11 @@ func syncServices(session *mgo.Session) bool {
 
 	fmt.Println("Uploading new Config to DB...")
 	c := session.DB("surv").C("services")
-	for  _, service := range config.Services {
+	for _, service := range config.Services {
 		service.Status = true
 		service.Change = time.Now()
-		service.THold = 0
 		err = c.Insert(service)
-		if(err != nil){
+		if err != nil {
 			fmt.Println(err)
 			return false
 		}
@@ -28,28 +28,28 @@ func syncServices(session *mgo.Session) bool {
 	return true
 }
 
-func serviceStatus(session *mgo.Session, id string, status bool){
+func serviceStatus(session *mgo.Session, id string, status bool) {
 	survs := session.DB("surv").C("services")
 
 	var S Service
 
 	err := survs.Find(bson.M{"_id": bson.ObjectIdHex(id)}).One(&S)
-	if(err != nil){
+	if err != nil {
 		fmt.Println(err)
 	}
 
-	if(S.Status != status){
-		if(status || time.Duration(S.THold) * time.Second < time.Since(S.Change)){
+	if S.Status != status {
+		if status || time.Duration(S.THold)*time.Second < time.Since(S.Change) {
 			go alert(S.Name, status)
 		}
 
-		if(status){
+		if status {
 			S.THold = 0
 		}
 		S.Status = status
 		S.Change = time.Now()
 		err = survs.Update(bson.M{"_id": S.Id}, &S)
-		if (err != nil) {
+		if err != nil {
 			fmt.Println(err)
 		}
 	}
